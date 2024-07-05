0360c9dd1c7e7db6d3c471926004beccb2a51d78
Oscar Bocanegra <85693186+obocanegra-dev@users.noreply.github.com>
2024-07-05 18:14:16 -0300

    Create 9_palindrome_number.py

---
+++ 9_palindrome_number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        
+        reversed_num = 0
+        original_x = x
+        
+        while x > reversed_num:
+            reversed_num = reversed_num * 10 + x % 10
+            x //= 10
+
+        return x == reversed_num or x == reversed_num // 10
